7c47083d489b5e4852da6426dfad37493d70de8f
= <=>
2021-04-09 00:00:39 +0700

    themaclass

--- app/src/main/java/com/example/test/Sinhvien.java
+++ app/src/main/java/com/example/test/Sinhvien.java
@@ -1,4 +1,13 @@
 package com.example.test;
 
 public class Sinhvien {
+    private String Ten;
+    private String Truong;
+    private String Noio;
+
+    public Sinhvien(String ten, String truong, String noio) {
+        Ten = ten;
+        Truong = truong;
+        Noio = noio;
+    }
 }
